e638b18bc5e9fa948a4985aacc24b56e03100e38
Tanvir Jawad <tjawad6912@bths.edu>
2022-05-21 17:23:30 -0400

    Added a WIP prototype of the Twitter thing

---
+++ server/twitterTest.py
@@ -0,0 +1,53 @@
+import urllib.parse
+import requests
+import json
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAPFecwEAAAAAvjnPLEh2Fbw1XmDaJWY2xb6Hrt0%3D6c79gIlmb1Pz61b0f01jc0vTDLejrfkvUvAUm94pA5rACKXXxr"
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+keywords = ["protest"]
+keywordQuery = ""
+
+for i in range(len(keywords) - 1):
+    keywordQuery += keywords
+    keywordQuery += "OR "
+keywordQuery += "#" + keywords[len(keywords) - 1]
+
+hashtags = ["BLM", "LGBTQ+"]
+hashtagQuery = ""
+
+for i in range(len(hashtags) - 1):
+    hashtagQuery += "#" + hashtags[i]
+    hashtagQuery += " OR "
+hashtagQuery += "#" + hashtags[len(hashtags) - 1]
+
+finalQuery = "(" + hashtagQuery + ")" + " (" + keywordQuery + ")"
+
+query_params = {'query': finalQuery}
+
+print(hashtagQuery)
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
